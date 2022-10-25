4c8068100dca3b2d32b042f3f2d753c7c7c0a86b
mauricio maciel <mau.maciel@gmail.com>
2022-10-24 22:57:37 -0300

    mi primer commit

---
+++ hola_mundo1.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class hola_mundo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner leer=new Scanner(System.in);
+       String nombre;
+        System.out.println("ingresa tu nombre ");
+        nombre=leer.next();
+        System.out.println("hola mundo soy "+nombre+" empezamos con java ");
+        
+        
+        // TODO code application logic here
+    }
+    
+}
